f4648d76acd8f473d2288e0bb863becbe5c5e697
rimsha-shoukat <rimsha.devs@gmail.com>
2026-01-04 16:16:22 +0500

    about  scroll animation added

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -2,8 +2,9 @@
 export function About() {
     return (
         <>
-            <section className="w-full  flex flex-row items-center justify-center">
-                <div className="w-[80vw] h-auto bg-[#f1f1f1] dark:bg-[#0a0a0a] border border-[#0a0a0a]/50 dark:border-[#f1f1f1]/50 rounded-md flex flex-row items-center justify-between p-10 gap-2">
+            <section className="w-full flex flex-row items-center justify-center">
+                <div className="aboutSection w-[80vw] h-auto bg-[#f1f1f1] dark:bg-[#0a0a0a] border border-[#0a0a0a]/50 dark:border-[#f1f1f1]/50 rounded-md flex flex-row items-center justify-between p-10 gap-2">
+                    {/* about me */}
                     <span className="w-1/2 flex flex-col items-start justify-center gap-5">
                         <h1 className="text-2xl font-bold line-clamp-2">Developing Clean, Scalable Web Applications</h1>
                         <p className="text-justify">I am a Front-End Developer with a solid foundation in HTML, CSS, JavaScript,
@@ -16,10 +17,12 @@ export function About() {
                             recognised certifications, and continuous learning. I take a structured and
                             detail-oriented approach to development and aim to contribute to teams that
                             value code quality, collaboration, and long-term scalability.</p>
+                        {/* resume link button */}
                         <button className="px-4 py-2 bg-black text-[#f1f1f1] dark:bg-white dark:text-[#0a0a0a] rounded-sm font-semibold hover:bg-[#0a0a0a]/90 dark:hover:bg-[#f1f1f1]/90 transition-all duration-300 ease-in-out">Click to view resume
-                            <svg className="inline pb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg>
+                            <svg className="inline pb-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg>
                         </button>
                     </span>
+                    {/* certificates */}
                     <span className="w-1/2 h-full flex items-center justify-center hover:scale-105 transition-all duration-300 ease-in-out">
                         <span className="absolute -rotate-6 -translate-x-1/3 -translate-y-8 w-65 h-auto border border-[#0a0a0a] dark:border-[#f1f1f1]/30 bg-[#f1f1f1] dark:bg-[#0a0a0a] rounded-sm">
                             <div className="w-full h-45 bg-[url('/assests/python.png')] bg-cover bg-no-repeat bg-center"></div>
